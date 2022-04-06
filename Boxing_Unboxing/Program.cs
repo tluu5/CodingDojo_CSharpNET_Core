60579f5ce628ad4965f1626a47d2eab13f4541e8
tluu5 <tluu5@uic.edu>
2022-04-06 07:31:43 -0500

    changes

---
+++ Boxing_Unboxing/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Boxing_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an empty List of type object
+            var boxed = new List<object>();
+
+            // Add the following values to the list: 7, 28, -1, true, "chair"
+            boxed.Add(7);
+            boxed.Add(28);
+            boxed.Add(-1);
+            boxed.Add(true);
+            boxed.Add("chair");
+
+            // Loop through the list and print all values (Hint: Type Inference might help here!)
+            int sum = 0;
+            foreach (var item in boxed)
+            {
+                Console.WriteLine(item);
+                if (item is int)
+                {
+                    Console.WriteLine("Looks like we have an int!");
+                    sum += (int)item;
+                    Console.WriteLine($"Current sum: {sum}");
+                }
+                // Add all values that are Int type together and output the sum
+            }
+        }
+    }
+}
--- Multiplication_Table/Program.cs
+++ Multiplication_Table/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Multiplication_Table
 {
@@ -7,6 +8,35 @@ namespace Multiplication_Table
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var table = BuildTable(10);
+            RenderTable(table);
+        }
+        public static int[,] BuildTable(int size)
+        {
+            int[,] table = new int[size, size];
+            for (var row = 0; row < size; row++)
+                for (var col = 0; col < size; col++)
+                    table[row, col] = (row + 1) * (col + 1);
+            return table;
+        }
+        public static void RenderTable(int[,] multiArray)
+        {
+            // StringBuilder class good for mutating strings
+            StringBuilder sb = new StringBuilder();
+
+            // gets length of specified dimention of multi array
+            int size = multiArray.GetLength(0);
+
+            for (var row = 0; row < size; row++)
+            {
+                sb.Append("[");
+                for (var col = 0; col < size; col++)
+                {
+                    sb.Append($"{multiArray[row, col]}, ");
+                }
+                sb.Append("]\n");
+            }
+            Console.WriteLine(sb);
         }
     }
 }
