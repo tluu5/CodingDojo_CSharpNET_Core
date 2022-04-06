0b0969cba65b93e3c7dc9e31d3688ed4583d6eca
tluu5 <tluu5@uic.edu>
2022-04-06 15:11:57 -0500

    changes

---  Human/Program.cs
+++  Human/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 
-namespace Human
+namespace HumanAssignment
 {
-    class Program
+    class Human
     {
-        static void Main(string[] args)
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health;
+
+        public int Health
         {
-            Console.WriteLine("Hello World!");
+            get { return health; }
+        }
+
+        public Human(string name)
+        {
+            Name = name;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            health = 100;
+        }
+
+        public Human(string name, int str, int intel, int dex, int hp)
+        {
+            Name = name;
+            Strength = str;
+            Intelligence = intel;
+            Dexterity = dex;
+            health = hp;
+        }
+
+        // Build Attack method
+        public int Attack(Human target)
+        {
+            int dmg = Strength * 3;
+            target.health -= dmg;
+            Console.WriteLine($"{Name} attacked {target.Name} for {dmg} damage!");
+            return target.health;
         }
     }
 }
