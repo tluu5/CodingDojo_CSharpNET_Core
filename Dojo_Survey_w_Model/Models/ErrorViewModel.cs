2a8b80e6976c20f4d3753baea6487e015600ee2b
tluu5 <tluu5@uic.edu>
2022-04-14 11:33:29 -0500

    Changes

---
+++ Dojo_Survey_w_Model/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dojo_Survey_w_Model.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
