3dc827b2157629e03cd9af7f20b3c8b0010842e3
tluu5 <tluu5@uic.edu>
2022-04-17 15:30:33 -0500

    Changes

--- sessionDemo/Controllers/HomeController.cs
+++ sessionDemo/Controllers/HomeController.cs
@@ -12,6 +12,8 @@ namespace sessionDemo.Controllers
 {
     public class HomeController : Controller
     {
+
+        public List<String> Names = new List<String>() {"qq","ww","ee","rr","tt","yy","uu","ii","oo","pp","aa","ss","dd","ff","gg","hh","jj","kk","ll","zz","xx","cc","vv","bb","nn","mm"};
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -21,11 +23,49 @@ namespace sessionDemo.Controllers
 
         public IActionResult Index()
         {
-            HttpContext.Session.SetString("name", "Brian");
-            string myName = HttpContext.Session.GetString("name");
-            Console.WriteLine(myName);
-            ViewBag.SessionName = myName;
-            return View("Index", myName);
+            // HttpContext.Session.SetString("name", "Brian");
+            // string myName = HttpContext.Session.GetString("name");
+            // Console.WriteLine(myName);
+            // ViewBag.SessionName = myName;
+            // if(myName == null){
+            //     Console.WriteLine("Nothing in session");
+            // } else {
+            //     Console.WriteLine(myName);
+            // }
+            if(HttpContext.Session.GetString("inSession") == null)
+            {
+                Console.WriteLine("There was nothing in session, creating a new one");
+                HttpContext.Session.SetString("inSession", "Working");
+                HttpContext.Session.SetString("CharName", Names[1]);
+                HttpContext.Session.SetInt32("CharLevel", 10);
+                HttpContext.Session.SetInt32("CharStr", 7);
+                HttpContext.Session.SetInt32("CharInt", 12);
+                HttpContext.Session.SetInt32("CharDex", 8);
+                HttpContext.Session.SetInt32("CharImg", 1);
+            } else {
+                Console.WriteLine("There was already a session.");
+            }
+            int? charImgNum = HttpContext.Session.GetInt32("CharImg");
+            ViewBag.CharImg = Url.Content($"~/images/{charImgNum}.jpeg");
+            ViewBag.CharName = HttpContext.Session.GetString("CharName");
+            ViewBag.CharLvl = HttpContext.Session.GetInt32("CharLevel");
+            ViewBag.CharStr = HttpContext.Session.GetInt32("CharStr");
+            ViewBag.CharInt = HttpContext.Session.GetInt32("CharInt");
+            ViewBag.CharDex = HttpContext.Session.GetInt32("CharDex");
+            return View("Index");
+        }
+
+        [HttpGet("generate")]
+        public IActionResult Generate()
+        {
+            Random rand = new Random();
+            HttpContext.Session.SetString("CharName", Names[rand.Next(0,Names.Count)]);
+            HttpContext.Session.SetInt32("CharLevel", rand.Next(1,11));
+            HttpContext.Session.SetInt32("CharStr", rand.Next(1,15));
+            HttpContext.Session.SetInt32("CharInt", rand.Next(1,15));
+            HttpContext.Session.SetInt32("CharDex", rand.Next(1,12));
+            HttpContext.Session.SetInt32("CharImg", rand.Next(1,5));
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
