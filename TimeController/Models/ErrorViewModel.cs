09bcaa77764e10ac46d5cdde442ea8b485345fb5
tluu5 <tluu5@uic.edu>
2022-04-19 08:43:29 -0500

    changes

---
+++ TimeController/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TimeController.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
